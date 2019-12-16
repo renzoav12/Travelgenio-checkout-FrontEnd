a4f386998cdf5f033da4dc990c86e84cc564c7a5
Lucas Baamonde <lucas.baamonde@travelgenio.com>
2019-12-16 13:01:04 -0300

    change routes

--- src/components/Root/root.routes.constants.ts
+++ src/components/Root/root.routes.constants.ts
@@ -1,4 +1,4 @@
 // tslint:disable:max-line-length
 export const ROUTE_HOME = '/hotels/checkout'
-export const ROUTE_NOT_FOUND = '/hotels/checkout/404/'
+export const ROUTE_NOT_FOUND = '/hotels/checkout/404'
 export const ROUTE_CHECKOUT = '/hotels/checkout/:productId'
