96d11afb41cd685803f98591e959a972c3ea0b6e
Alejandro Kullmer <alejandro.kullmer@travelgenio.com>
2020-03-10 09:15:15 -0300

    Add MealPlan type

--- src/components/MealPlan/MealPlan.tsx
+++ src/components/MealPlan/MealPlan.tsx
@@ -33,22 +33,16 @@ const useStyles = makeStyles((theme: Theme) =>
     mealPlan: {
       backgroundColor: "#E9EFF9",
       color: "#3D4355"
-
     },
     allInclusive: {
       backgroundColor: "#028A51",
       color: "white"
     },
-    onlyRoom: {
-      backgroundColor: "#E9EFF9",
-      color: "#3D4355",
-      paddingLeft:15
-    },
     mealPlanIcon: {
       width: 30,
       height: 30,
       marginLeft: 18,
-      marginTop:-20
+      marginTop: -8
     },
     mealPlanDescription: {
       paddingLeft: 10,
@@ -61,14 +55,12 @@ const MealPlan: FunctionComponent<MealPlanProps> = props => {
   const classes = useStyles();
 
   const mealPlanTypes: Map<String, MealPlanType> = new Map([
-    ["all_inclusive", {icon: <AllInclusive className={classes.mealPlanIcon}/>, class: classes.allInclusive}],
-    ["half_board", {icon: <Meal className={classes.mealPlanIcon}/>, class: classes.mealPlan}],
-    ["full_board", {icon: <Meal className={classes.mealPlanIcon}/>, class: classes.mealPlan}],
-    ["breakfast", {icon: <Breakfast className={classes.mealPlanIcon}/>, class: classes.mealPlan}],
-    ["room_only", {icon: null, class: classes.onlyRoom}]
+    ["ALL_INCLUSIVE", {icon: <AllInclusive className={classes.mealPlanIcon}/>, class: classes.allInclusive}],
+    ["OTHER", {icon: <Meal className={classes.mealPlanIcon}/>, class: classes.mealPlan}],
+    ["BREAKFAST", {icon: <Breakfast className={classes.mealPlanIcon}/>, class: classes.mealPlan}]
   ]);
   
-  const mealPlanType: MealPlanType | undefined = mealPlanTypes.get("room_only"/*props.type*/);
+  const mealPlanType: MealPlanType | undefined = mealPlanTypes.get(props.type);
 
   if(mealPlanType) {
     return <Box className={classes.container}>
