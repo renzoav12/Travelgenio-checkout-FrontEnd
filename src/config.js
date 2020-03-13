b5d32f36f7430c55e07003a11cef6d877be58118
Renzo <renzo.avila@travelgenio.com>
2020-03-13 10:09:14 -0300

    change url checkout

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const dev = {
     PRODUCT_API: 'https://dev-hotels-shopping-product.travelagency.tech',
-    CHECKOUT_API: 'https://dev-hotels-shopping-checkout.travelagency.tech'
+    CHECKOUT_API: 'https://dev-hotels-shopping-checkout.travelagency.tech/checkout/hotels/'
 };
 
 const prod = {
     PRODUCT_API: "https://" + window.location.hostname + "/hotels/api/",
-    CHECKOUT_API: "https://" + window.location.hostname + "/hotels/api/"
+    CHECKOUT_API: "https://" + window.location.hostname + "/checkout/hotels/"
 };
 
 const config = process.env.REACT_APP_STAGE === 'prod'
